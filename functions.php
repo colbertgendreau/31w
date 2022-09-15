62b2e51ed494e7d77f5fd5a8ea9b8072b67ecdfc
colbertgendreau <97406341+colbertgendreau@users.noreply.github.com>
2022-09-15 11:27:49 -0400

    modification de wp_enqueue_style

--- functions.php
+++ functions.php
@@ -51,7 +51,16 @@ add_action( 'after_setup_theme', 'under_setup' );
  * Enqueue scripts and styles.
  */
 function under_scripts() {
-	wp_enqueue_style( 'under-style', get_stylesheet_uri(), array(), _S_VERSION );
+	// wp_enqueue_style( 'under-style', 
+	// 				get_stylesheet_uri(), 
+	// 				array(), 
+	// 				_S_VERSION );
+
+	wp_enqueue_style('under-style', 
+					get_template_directory_uri() . '/style.css', 
+					array(), 
+					filemtime(get_template_directory() . '/style.css'), 
+					false);
 }
 add_action( 'wp_enqueue_scripts', 'under_scripts' );
 
