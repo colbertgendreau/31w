bd776209f9ebd50e8064ad920d492eefaa39267a
colbertgendreau <97406341+colbertgendreau@users.noreply.github.com>
2022-09-20 17:06:18 -0400

    Ajout de styles dans les fichiers .scss

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ get_header(); ?>
             if ( have_posts() ) :
                 while ( have_posts() ) :
                     the_post();
-                    the_title('<h1>', '</h1>');
+                    the_title('<h2>', '</h2>');
                     the_content(null, true);
                 endwhile;
             endif;
